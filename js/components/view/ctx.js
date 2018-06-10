d49575991edabd30e495a2dcecce46e568f96907
vencax <vencax@centrum.cz>
2018-06-10 16:18:03 +0200

    style

--- js/components/view/ctx.js
+++ js/components/view/ctx.js
@@ -9,8 +9,8 @@ const Preview = observer(({cv}) => {
 const UnObservedEditor = ({cv, ...rest}) => {
   return (
     <div>
-      <div style={{width: '25%', float: 'right'}}><Preview cv={cv} /></div>
-      <div style={{width: '70%'}}><CMirrorEditor value={cv.editedVal} {...rest} /></div>
+      <div style={{width: '25%', float: 'right', marginRight: '1em'}}><Preview cv={cv} /></div>
+      <div style={{width: '74%'}}><CMirrorEditor value={cv.editedVal} {...rest} /></div>
     </div>
   )
 }
